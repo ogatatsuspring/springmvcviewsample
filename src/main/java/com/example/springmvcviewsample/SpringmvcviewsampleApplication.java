0178eb4f326e79d22988a4d5e1e1794e1d5fe054
ogawatatsuya <ogawatatsuya@localhost>
2025-08-02 21:25:57 +0900

    FirstCommit

---
+++ src/main/java/com/example/springmvcviewsample/SpringmvcviewsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcviewsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcviewsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcviewsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmvcviewsample/SpringmvcviewsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcviewsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcviewsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
