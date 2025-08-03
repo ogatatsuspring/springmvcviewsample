e2d4a1e687f7ac38140676317e91d60930a6bf20
ogawatatsuya <ogawatatsuya@localhost>
2025-08-03 11:13:40 +0900

    Model, ModelAndView, Thymeleafを導入

--- src/main/java/com/example/springmvcviewsample/controller/HelloViewController.java
+++ src/main/java/com/example/springmvcviewsample/controller/HelloViewController.java
@@ -1,16 +1,31 @@
 package com.example.springmvcviewsample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping( "/hello" )
+//@RequestMapping( "/hello" )
 public class HelloViewController {
   
-  @GetMapping( "/view" )
+//  @GetMapping( "/view" )
+  @GetMapping( "/hello/view" )
   public String helloView() {
     return "hello";
+  }
+  
+  @GetMapping( "/hello/model" )
+  public String helloView( Model model ) {
+    model.addAttribute( "msg", "タイムリーフ！！！(Model)" );
+    return "helloThymeleaf";
+  }
+  
+  @GetMapping( "/hello/modelandview" )
+  public ModelAndView helloView( ModelAndView mav ) {
+    mav.addObject( "msg", "タイムリーフ！！！(ModelAndView)" );
+    mav.setViewName( "helloThymeleaf" );
+    return mav;
   }
 
 }
